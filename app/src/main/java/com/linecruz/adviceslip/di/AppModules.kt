17da9848d906c9af94874e36bdd6071e1d70687b
aline <linegcruz@gmail.com>
2022-08-09 17:21:19 -0300

    feat(advice_slip): added DI with koin.

---
+++ app/src/main/java/com/linecruz/adviceslip/di/AppModules.kt
@@ -0,0 +1,18 @@
+package com.linecruz.adviceslip.di
+
+import com.linecruz.adviceslip.data.repository.AdviceSlipRepositoryImpl
+import com.linecruz.adviceslip.data.service.AdviceSlipService
+import com.linecruz.adviceslip.data.service.AdviceSlipServiceImpl
+import com.linecruz.adviceslip.domain.repository.AdviceSlipRepository
+import com.linecruz.adviceslip.domain.usecase.FetchAdviceSlipUseCase
+import com.linecruz.adviceslip.presentation.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModules = module {
+    single { AdviceSlipServiceImpl().getService() }
+    single<AdviceSlipRepository> { AdviceSlipRepositoryImpl(service = get()) }
+    factory{ FetchAdviceSlipUseCase(repository = get()) }
+
+    viewModel { MainViewModel(useCase = get()) }
+}
