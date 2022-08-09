7b1c3cd917a99573f8a7e4c54f786cad0696381e
aline <linegcruz@gmail.com>
2022-08-09 10:17:25 -0300

    feat(advice_slip): added functions to fill advice and show when error ocurred.

--- app/src/main/java/com/linecruz/adviceslip/presentation/MainActivity.kt
+++ app/src/main/java/com/linecruz/adviceslip/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.linecruz.adviceslip.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.linecruz.adviceslip.R
 import com.linecruz.adviceslip.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupButtonListener()
+        setupViewModel()
+    }
+
+    private fun setupButtonListener() {
+        binding.buttonMainSeekAdvice.setOnClickListener {
+            //viewmodel.fetchAdvice()
+        }
+    }
+
+    private fun setupViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    private fun fillAdviceSlip(message: String) {
+        binding.textMainAdviceSlip.text = message
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(
+            this, getString(R.string.label_error_ocurred, message), Toast.LENGTH_SHORT
+        ).show()
     }
 }
